ad40dbed2c3ae2f38bda2aee6ea06bddda583a87
lenci123 <lenart.golob17@gmail.com>
2019-09-29 21:14:47 +0200

    new commit

--- login.php
+++ login.php
@@ -77,7 +77,7 @@ include_once "header.php";
               </div>
               <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
               <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
+              <a href="g-callback.php"><button class="btn btn-lg btn-google btn-block text-uppercase"><i class="fab fa-google mr-2"></i> Sign in with Google</button></a>
               <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
             </form>
           </div>
